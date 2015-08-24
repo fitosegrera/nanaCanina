6a09dc07e3a1910c854bfaf0c790a40434fc0306
fitosegrera <fitosegrera@gmail.com>
2015-08-24 21:15:05 +0800

    x

--- js/app.js
+++ js/app.js
@@ -1,19 +1,11 @@
-/* Custom Javascript for this PhoneGap APP */
-
-document.addEventListener("deviceready",onDeviceReady,false);
-
-function onDeviceReady()
-{
-  //Phonegap is ready
-  console.log("Phonegap is ready");
-}
-
-$(document).on( "mobileinit", function() {
-	console.log("Initialize jQuery Mobile Phonegap Enhancement Configurations")
-    // Make your jQuery Mobile framework configuration changes here!
-    $.mobile.allowCrossDomainPages = true;
-    $.support.cors = true;
-    $.mobile.buttonMarkup.hoverDelay = 0;
-    $.mobile.pushStateEnabled = false;
-    $.mobile.defaultPageTransition = "none";
+$(document).ready(function(){
+	$('#reader').html5_qrcode(function(data){
+			$('#read').html(data);
+		},
+		function(error){
+			$('#read_error').html(error);
+		}, function(videoError){
+			$('#vid_error').html(videoError);
+		}
+	);
 });
