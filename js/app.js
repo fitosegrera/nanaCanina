37fab68d695207b2ce5ed516513d1b164503f338
fitosegrera <fitosegrera@gmail.com>
2015-08-24 22:56:37 +0800

    qrcode test

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
-$(document).ready(function(){
-	$('#reader').html5_qrcode(function(data){
-			$('#read').html(data);
-		},
-		function(error){
-			$('#read_error').html(error);
-		}, function(videoError){
-			$('#vid_error').html(videoError);
-		}
-	);
-});
+var resultDiv;
+
+document.addEventListener("deviceready", init, false);
+function init() {
+    document.querySelector("#startScan").addEventListener("touchend", startScan, false);
+    resultDiv = document.querySelector("#results");
+}
+
+function startScan() {
+
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            var s = "Result: " + result.text + "<br/>" +
+            "Format: " + result.format + "<br/>" +
+            "Cancelled: " + result.cancelled;
+            resultDiv.innerHTML = s;
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        }
+    );
+
+}
