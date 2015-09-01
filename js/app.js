d1c98b1002a94c6d7189b0adcb6291b0f75c94e7
fitosegrera <fitosegrera@gmail.com>
2015-09-01 21:43:44 +0800

    added qr code

--- js/app.js
+++ js/app.js
@@ -10,10 +10,15 @@ function startScan() {
 
     cordova.plugins.barcodeScanner.scan(
         function (result) {
+            var url = "http://fii.to:3000/mascotas?data="+result.text
             var s = "Result: " + result.text + "<br/>" +
             "Format: " + result.format + "<br/>" +
             "Cancelled: " + result.cancelled;
-            resultDiv.innerHTML = s;
+            resultDiv.innerHTML = url;
+            var req = new XMLHttpRequest();
+            req.open("GET", "http://fii.to:3000/mascotas?data="+result.text, false);
+            req.send(null);
+            console.log(req.responseText);
         },
         function (error) {
             alert("Scanning failed: " + error);
