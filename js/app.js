05cb91d28657bf6b4758b9018c31d84441b47bd6
fitosegrera <fitosegrera@gmail.com>
2015-09-01 22:31:54 +0800

    added qr code

--- js/app.js
+++ js/app.js
@@ -1,26 +1,29 @@
 var resultDiv;
 
-document.addEventListener("deviceready", init, false);
+document.addEventListener("deviceready", init, false)
+
 function init() {
-    document.querySelector("#scan").addEventListener("touchend", startScan, false);
-    resultDiv = document.querySelector("#results");
+    document.querySelector("#scan").addEventListener("touchend", startScan, false)
+    resultDiv = document.querySelector("#results")
 }
 
 function startScan() {
 
     cordova.plugins.barcodeScanner.scan(
-        function (result) {
-            var url = "http://fii.to:3000/mascotas?data="+result.text
+        function(result) {
+            var url = "http://fii.to:3000/mascotas?data=" + result.text
             var s = "Result: " + result.text + "<br/>" +
-            "Format: " + result.format + "<br/>" +
-            "Cancelled: " + result.cancelled;
-            resultDiv.innerHTML = url;
-            var req = new XMLHttpRequest();
-            req.open("GET", "http://fii.to:3000/mascotas?data="+result.text, false);
-            req.send(null);
-            console.log(req.responseText);
+                "Format: " + result.format + "<br/>" +
+                "Cancelled: " + result.cancelled
+
+            var req = new XMLHttpRequest()
+            req.open("GET", "http://fii.to:3000/mascotas?data=" + result.text, false)
+            req.send(null)
+            console.log(req.responseText)
+            var r = req.responseText
+            resultDiv.innerHTML = r
         },
-        function (error) {
+        function(error) {
             alert("Scanning failed: " + error);
         }
     );
